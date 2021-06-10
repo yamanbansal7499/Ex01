c1f5537eb7810bead2a8a0560d4d2c1cf68790fd
yamanbansal7499 <yamanbansal7499@gmail.com>
2021-06-10 11:58:18 +0530

    Second Commit

---
+++ Models/Emp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ex01.Models
+{
+    [Table("Emp")]
+    public class Emp
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesign { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
